ffddae38e246b871264e8bd478cd8d758bda4a1c
SHAKOTN <shakotn@protonmail.com>
2022-12-02 18:47:47 +0200

    feat: add dummy test to prove foundry works

---
+++ packages/contracts/test/foundry/Contract.t.sol
@@ -0,0 +1,19 @@
+pragma solidity 0.8.10;
+
+import "forge-std/Test.sol";
+
+contract DummyTest is Test {
+    uint256 testNumber;
+
+    function setUp() public {
+        testNumber = 42;
+    }
+
+    function testNumberIs42() public {
+        assertEq(testNumber, 42);
+    }
+
+    function testFailSubtract43() public {
+        testNumber -= 43;
+    }
+}
