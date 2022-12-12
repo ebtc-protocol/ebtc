1fb3baf031454874736836515d7c0a1c65d790c9
SHAKOTN <shakotn@protonmail.com>
2022-12-12 15:19:59 +0200

    chore: use dynamic borrowed eBTC amount in test; add more assertions

--- packages/contracts/foundry_test/MultiCDPFuzz.t.sol
+++ packages/contracts/foundry_test/MultiCDPFuzz.t.sol
@@ -7,9 +7,7 @@ import {Utilities} from "./utils/Utilities.sol";
 
 contract CDPTest is eBTCBaseFixture {
     uint256 private testNumber;
-    address user;
     Utilities internal utils;
-    address payable[] internal users;
 
     function setUp() public override {
         eBTCBaseFixture.setUp();
@@ -19,20 +17,27 @@ contract CDPTest is eBTCBaseFixture {
         user = msg.sender;
         utils = new Utilities();
     }
-
     /* Open CDPs for fuzzed amount of users
+    BorrowedAmount is calculated as: (Collateral * eBTC Price) / CR
     */
     function testCdpsForManyUsers(uint8 amountUsers) public {
+        // Skip case when amount of Users is 0
+        vm.assume(amountUsers > 1);
+
+        // Populate users
+        address payable[] memory users;
         users = utils.createUsers(amountUsers);
+
         uint collateral = 30 ether;
+        uint collateralRatio = 160e16;  // 160% take higher CR as CCR is 150%
+        uint borrowedAmount = collateral.mul(priceFeedMock.fetchPrice()).div(collateralRatio);
         // Iterate thru all users and open CDP for each of them
         for (uint userIx = 0; userIx < users.length; userIx++) {
             vm.deal(users[userIx], 300 ether);
             vm.prank(users[userIx]);
             borrowerOperations.openCdp{value : collateral}(
                 5e17,
-                // TODO: Minted eBTC value should change as we will use another CL for btc/eth prices
-                1800e18,
+                borrowedAmount,
                 "some hint",
                 "some hint"
             );
