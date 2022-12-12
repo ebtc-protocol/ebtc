69538bfcb99c2942c98e77faa7496ff237083ac0
SHAKOTN <shakotn@protonmail.com>
2022-12-12 14:42:41 +0200

    feat: add Utilities.sol for users creation;  add one more test for unhappy case with ICR < MCR; add first fuzz test for many CDPs opened for users

--- packages/contracts/foundry_test/MultiCDPFuzz.t.sol
+++ packages/contracts/foundry_test/MultiCDPFuzz.t.sol
@@ -3,11 +3,13 @@ pragma solidity 0.6.11;
 
 import "forge-std/Test.sol";
 import {eBTCBaseFixture} from "./BaseFixture.sol";
-
+import {Utilities} from "./utils/Utilities.sol";
 
 contract CDPTest is eBTCBaseFixture {
     uint256 private testNumber;
     address user;
+    Utilities internal utils;
+    address payable[] internal users;
 
     function setUp() public override {
         eBTCBaseFixture.setUp();
@@ -15,17 +17,29 @@ contract CDPTest is eBTCBaseFixture {
         eBTCBaseFixture.connectCoreContracts();
         eBTCBaseFixture.connectLQTYContractsToCore();
         user = msg.sender;
-        vm.deal(user, 300 ether);
+        utils = new Utilities();
     }
-//    function testCdpsAmounts(uint8 cdpAmount) public {
-//        for (uint i = 0; i < cdpAmount; i++) {
-//            vm.prank(user);
-//            borrowerOperations.openCdp{value : address(user).balance}(
-//                5e17,
-//                170e20,
-//                "some hint",
-//                "some hint"
-//            );
-//        }
-//    }
+
+    /* Open CDPs for fuzzed amount of users
+    */
+    function testCdpsForManyUsers(uint8 amountUsers) public {
+        users = utils.createUsers(amountUsers);
+        uint collateral = 30 ether;
+        // Iterate thru all users and open CDP for each of them
+        for (uint userIx = 0; userIx < users.length; userIx++) {
+            vm.deal(users[userIx], 300 ether);
+            vm.prank(users[userIx]);
+            borrowerOperations.openCdp{value : collateral}(
+                5e17,
+                // TODO: Minted eBTC value should change as we will use another CL for btc/eth prices
+                1800e18,
+                "some hint",
+                "some hint"
+            );
+            // Make sure that each user has now CDP opened
+            assertEq(sortedCdps.cdpCountOf(users[userIx]), 1);
+        }
+        // Make sure amount of SortedCDPs equals to `amountUsers`
+        assertEq(sortedCdps.getSize(), amountUsers);
+    }
 }
