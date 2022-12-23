275f8f41bd421f2c11c44e63f6f8e77c2f0f7784
SHAKOTN <shakotn@protonmail.com>
2022-12-23 12:34:22 +0200

    chore: add getCdpsOf function WIP

---
+++ packages/contracts/foundry_test/SortedCdps.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.6.11;
+
+import "forge-std/Test.sol";
+import {eBTCBaseFixture} from "./BaseFixture.sol";
+import {Utilities} from "./utils/Utilities.sol";
+
+
+contract CDPOpsTest is eBTCBaseFixture {
+    Utilities internal _utils;
+
+    function setUp() public override {
+        eBTCBaseFixture.setUp();
+        eBTCBaseFixture.connectLQTYContracts();
+        eBTCBaseFixture.connectCoreContracts();
+        eBTCBaseFixture.connectLQTYContractsToCore();
+        _utils = new Utilities();
+    }
+}
