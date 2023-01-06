8760b76d9d6899482f89815a4532721944f1639d
Entreprenerd <alex@entreprenerd.xyz>
2023-01-06 17:57:31 +0100

    feat: made FL config public

--- packages/contracts/contracts/Dependencies/ERC3156FlashLender.sol
+++ packages/contracts/contracts/Dependencies/ERC3156FlashLender.sol
@@ -6,10 +6,10 @@ import "../Interfaces/IERC3156FlashLender.sol";
 import "../Interfaces/IWETH.sol";
 
 abstract contract ERC3156FlashLender is IERC3156FlashLender {
-    // TODO: Fix
-    address internal constant FEE_RECIPIENT = address(1);
-    uint256 internal constant FEE_AMT = 50; // 50 BPS
-    uint256 internal constant MAX_BPS = 10_000;
+    // TODO: Fix / Finalize
+    address public constant FEE_RECIPIENT = address(1);
+    uint256 public constant FEE_AMT = 50; // 50 BPS
+    uint256 public constant MAX_BPS = 10_000;
 
     // NOTE: Mainnet WETH
     IWETH public constant WETH = IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
