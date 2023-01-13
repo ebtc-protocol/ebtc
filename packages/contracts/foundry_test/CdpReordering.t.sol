d21dc3be888180f5c2ee5643b990aeca6b43ecaa
dapp-whisperer <dapp-whisperer@pm.me>
2023-01-12 19:11:18 -0500

    add reinsertion order test, cleanup

--- packages/contracts/foundry_test/CdpReordering.t.sol
+++ packages/contracts/foundry_test/CdpReordering.t.sol
@@ -34,6 +34,8 @@ contract CdpReorderingTest is eBTCBaseFixture, LogUtils {
     uint256 public constant DECIMAL_PRECISION = 1e18;
     uint256 internal constant MAX_UINT256 = 2**256 - 1;
 
+    bool constant DEBUG = false; // Print debug logging
+
     ////////////////////////////////////////////////////////////////////////////
     // Tests
     ////////////////////////////////////////////////////////////////////////////
@@ -56,54 +58,128 @@ contract CdpReorderingTest is eBTCBaseFixture, LogUtils {
         We want to confirm they are _not reordered_ due to pending interest payments
         - Ensure the order is the same at the start as at the end
     */
-    function testTroveReorderingSim() public {
+    function testCdpOrderingSanityCheck() public {
+        uint256 cdp0Debt = 2000e18;
+        uint256 cdp1Debt = 2001e18;
+
+        bytes32 cdp0Id = borrowerOperations.openCdp{value: _calculateCollAmount(cdp0Debt, 200e16)}(
+            5e17,
+            cdp0Debt,
+            bytes32(0),
+            bytes32(0)
+        );
+
+        // Insert a second CDP with a lower CR immediately. We expect it to be later in the LL.
+        bytes32 cdp1Id = borrowerOperations.openCdp{value: _calculateCollAmount(cdp0Debt, 200e16)}(
+            5e17,
+            cdp1Debt,
+            bytes32(0),
+            bytes32(0)
+        );
+
+        bytes32 first = sortedCdps.getFirst();
+        assertEq(first, cdp0Id);
+        bytes32 second = sortedCdps.getNext(first);
+        assertEq(second, cdp1Id);
     }
 
     /**
-        Pending interest should be considered when inserting a new CDP
-        Create a CDP.
-        Later create a second trove that without interest, would be inserted _after_ the original CDP. However, considering the interest the first has accumulated, it should be inserted before. Ensure this happens.
+        Pending interest should be considered when inserting a new CDP. CDPs with a lower CR should be closer to the end of the LL
+        - Create a CDP.
+        - Create a second CDP with a lower CR than the first one after some time has passed for interest to accumulate
+        - If pending interest was not considered, this CDP should go after the first one (it has a lower CR so goes later in the list)
+        - If pending interest is considered, this new CDP should go before the first one (it has a higher CR now, due to the increased debt)
      */
     function testPendingInterestShouldBeConsideredWhenInsertingNewCdp() public {
-        uint cdp0Debt = 2000e18;
-        uint cdp1Debt = 2001e18;
+        uint256 cdp0Debt = 2000e18;
+        uint256 cdp1Debt = 2001e18;
 
         bytes32 cdp0Id = borrowerOperations.openCdp{value: _calculateCollAmount(cdp0Debt, 200e16)}(
-                5e17,
-                cdp0Debt,
-                bytes32(0),
-                bytes32(0)
-            );
+            5e17,
+            cdp0Debt,
+            bytes32(0),
+            bytes32(0)
+        );
 
-        console.log("Initial Cdp0 State");
-        _getAndPrintCdpState(cdp0Id);
+        if (DEBUG) {
+            console.log("Initial Cdp0 State");
+            _getAndPrintCdpState(cdp0Id);
+        }
 
         skip(365 days);
 
-        console.log("Cdp0 State After Timewarp");
-        _getAndPrintCdpState(cdp0Id);
-
+        // Insert a second CDP with a lower CR than the first one originally, but with a higher CR due to the interest. It should be earlier in the list given the higher CR
         bytes32 cdp1Id = borrowerOperations.openCdp{value: _calculateCollAmount(cdp0Debt, 200e16)}(
-                5e17,
-                cdp1Debt,
-                bytes32(0),
-                bytes32(0)
-            );
-        
-        console.log("Initial Cdp1 State");
-        _getAndPrintCdpState(cdp1Id);
+            5e17,
+            cdp1Debt,
+            bytes32(0),
+            bytes32(0)
+        );
 
         bytes32 first = sortedCdps.getFirst();
         assertEq(first, cdp1Id);
         bytes32 second = sortedCdps.getNext(first);
         assertEq(second, cdp0Id);
+
+        if (DEBUG) {
+            console.log("Cdp0 State After Timewarp");
+            _getAndPrintCdpState(cdp0Id);
+            _printCdpList();
+            console.log("Initial Cdp1 State");
+            _getAndPrintCdpState(cdp1Id);
+
+            console.log(cdpManager.getNominalICR(cdp0Id));
+            console.log(cdpManager.getNominalICR(cdp1Id));
+        }
+        
     }
 
     /**
-        Open two CDPs of random sizes at the same time.
-        - How long does it take for compounding interest to cause them to flip?
+        - Open two CDPs at the same time. 
+        - Advance time such that pending interest can accumulate 
+        - Modify 
+    */
+    function testPendingInterestShouldBeConsideredWhenReinsertingExistingCdp() public {
+        uint256 cdp0Debt = 2000e18;
+        uint256 cdp1Debt = 2001e18;
+
+        bytes32 cdp0Id = borrowerOperations.openCdp{value: _calculateCollAmount(cdp0Debt, 200e16)}(
+            5e17,
+            cdp0Debt,
+            bytes32(0),
+            bytes32(0)
+        );
+
+        bytes32 cdp1Id = borrowerOperations.openCdp{value: _calculateCollAmount(cdp0Debt, 200e16)}(
+            5e17,
+            cdp1Debt,
+            bytes32(0),
+            bytes32(0)
+        );
+
+        skip(365 days);
+
+        // CDP 1 has more debt and same coll, therefore a lower CR - it should be further towards the end of the LL (0 -> 1)
+        bytes32 first = sortedCdps.getFirst();
+        assertEq(first, cdp0Id);
+        bytes32 second = sortedCdps.getNext(first);
+        assertEq(second, cdp1Id);
+
+        // Update Operation on one that should cause a reorder
+        // We will make CDP 1 have less debt, raising it's CR above that of CDP 0
+        borrowerOperations.addColl{value: 1e18}(cdp1Id, bytes32(0), bytes32(0));
+
+        // They should have switched (1 -> 0)
+        first = sortedCdps.getFirst();
+        assertEq(first, cdp1Id);
+        second = sortedCdps.getNext(first);
+        assertEq(second, cdp0Id);
+    }
+
+    /**
+        Open two CDPs of set sizes at the same time.
+        - Does compounding interest to cause them to flip?
         - What is the relative different in ICR/NICR of them at the maximum time?
-        - Do they flip within the maximum time alloted? (100 years)
     */
     function testCdpReorderingTwoSameTimeSim() public {
         uint256 numCdps = 2;
@@ -133,21 +209,28 @@ contract CdpReorderingTest is eBTCBaseFixture, LogUtils {
         uint256[] memory cdpNICRBefore = new uint256[](numCdps);
         uint256[] memory cdpNICRAfter = new uint256[](numCdps);
 
-        uint timeElapsed = 0;
-        uint maxTimeElapsed = 300 * 365 days;
+        uint256 timeElapsed = 0;
+        uint256 maxTimeElapsed = 300 * 365 days;
 
-        _getAndPrintCdpState(cdpIds[0]);
-        _getAndPrintCdpState(cdpIds[1]);
+        if (DEBUG) {
+            _getAndPrintCdpState(cdpIds[0]);
+            _getAndPrintCdpState(cdpIds[1]);
+        }
 
         while (timeElapsed < maxTimeElapsed) {
             // Track ICR and NICR
             cdpNICRBefore[0] = cdpManager.getNominalICR(cdpIds[0]);
             cdpNICRBefore[1] = cdpManager.getNominalICR(cdpIds[1]);
 
-            console.log("====== Time Passed ", timeElapsed / 365 days ," ======");
-            console.log("NICR Before [0] ", format(cdpNICRBefore[0]));
-            console.log("NICR Before [1] ", format(cdpNICRBefore[1]));
-            console.log("NICR Before Diff", format(stdMath.delta(cdpNICRBefore[0], cdpNICRBefore[1])));
+            if (DEBUG) {
+                console.log("====== Time Passed ", timeElapsed / 365 days, " ======");
+                console.log("NICR Before [0] ", format(cdpNICRBefore[0]));
+                console.log("NICR Before [1] ", format(cdpNICRBefore[1]));
+                console.log(
+                    "NICR Before Diff",
+                    format(stdMath.delta(cdpNICRBefore[0], cdpNICRBefore[1]))
+                );
+            }
 
             // Advance Time
             skip(365 days);
@@ -156,10 +239,12 @@ contract CdpReorderingTest is eBTCBaseFixture, LogUtils {
             cdpNICRAfter[0] = cdpManager.getNominalICR(cdpIds[0]);
             cdpNICRAfter[1] = cdpManager.getNominalICR(cdpIds[1]);
 
-            console.log("NCIR After [0] ", format(cdpNICRAfter[0]));
-            console.log("NCIR After [1] ", format(cdpNICRAfter[1]));
-            console.log("NICR After Diff", format(stdMath.delta(cdpNICRAfter[0], cdpNICRAfter[1])));
-            console.log("");
+            if (DEBUG) {
+                console.log("NCIR After [0] ", format(cdpNICRAfter[0]));
+                console.log("NCIR After [1] ", format(cdpNICRAfter[1]));
+                console.log("NICR After Diff", format(stdMath.delta(cdpNICRAfter[0], cdpNICRAfter[1])));
+                console.log("");
+            }
 
             // If it ordering (NICR) has reversed since last check, report
             if (
@@ -169,7 +254,7 @@ contract CdpReorderingTest is eBTCBaseFixture, LogUtils {
                 console.log("Reorder!");
                 return;
             }
-                timeElapsed = timeElapsed + 365 days;
+            timeElapsed = timeElapsed + 365 days;
         }
     }
 
@@ -199,15 +284,57 @@ contract CdpReorderingTest is eBTCBaseFixture, LogUtils {
         _printCdpState(cdpId, cdpState);
     }
 
+    function _bulkOpenCdps(
+        uint256[] memory debts,
+        uint256[] memory colls,
+        address owner
+    ) internal returns (bytes32[] memory cdpIds) {
+        require(debts.length == colls.length, "Input array length mismatch");
+
+        vm.startPrank(owner);
+        for (uint256 i = 0; i < debts.length; i++) {
+            bytes32 cdpId = borrowerOperations.openCdp{value: colls[i]}(
+                5e17,
+                debts[i],
+                bytes32(0),
+                bytes32(0)
+            );
+
+            cdpIds[i] = cdpId;
+        }
+        vm.stopPrank();
+
+        return cdpIds;
+    }
+
+    function _printCdpList() internal {
+        // TODO: Add print functions to sorted CDPs? Paginated optionally?
+        uint numCdps = sortedCdps.getSize();
+        uint i = 0;
+        bytes32 cdpId;
+
+        while (i < numCdps) {
+            if (i == 0) {
+                cdpId = sortedCdps.getFirst();
+            }
+            else {
+                cdpId = sortedCdps.getNext(cdpId);
+            }
+            console.log(vm.toString(cdpId));
+            ++i;
+        }
+    }
+
     function _printCdpState(bytes32 cdpId, CdpState memory cdpState) internal {
         // Issue console.log bytes32
         console.log("=== CDP State for ===");
-        console.log("debt", format(cdpState.debt)); 
+        console.log("debt", format(cdpState.debt));
         console.log("coll", format(cdpState.coll));
-        console.log("pendingEBTCDebtReward", format(cdpState.pendingEBTCDebtReward)); 
-        console.log("pendingEBTCDebtInterest", format(cdpState.pendingEBTCInterest)); 
+        console.log("pendingEBTCDebtReward", format(cdpState.pendingEBTCDebtReward));
+        console.log("pendingEBTCDebtInterest", format(cdpState.pendingEBTCInterest));
         console.log("pendingETHReward", format(cdpState.pendingETHReward));
         console.log("ICR", format(cdpManager.getCurrentICR(cdpId, priceFeedMock.getPrice())));
+        console.log("NICR", format(cdpManager.getNominalICR(cdpId)));
         console.log("");
     }
 }
