5cc7e729af47ff9b2056e2de2d917e0a7be3ba9b
rayeaster <rayeaster@hotmail.com>
2023-04-25 21:10:19 +0800

    fix solidity coverage compile error

--- packages/contracts/contracts/TestContracts/DefaultPoolTester.sol
+++ packages/contracts/contracts/TestContracts/DefaultPoolTester.sol
@@ -5,7 +5,6 @@ pragma solidity 0.8.17;
 import "../DefaultPool.sol";
 
 contract DefaultPoolTester is DefaultPool {
-
     constructor(
         address _cdpManagerAddress,
         address _activePoolAddress,
