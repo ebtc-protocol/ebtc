c403afb59e42b2689ab6241e1957f992d7c8e86f
dapp-whisperer <dapp@badger.com>
2023-05-07 11:08:32 -0400

    test_GetUsersByRoleReturnsExpected

--- packages/contracts/foundry_test/Governor.fundamentals.t.sol
+++ packages/contracts/foundry_test/Governor.fundamentals.t.sol
@@ -49,31 +49,60 @@ contract GovernorFundamentalsTest is eBTCBaseFixture {
     // Setup: Assign a few addresses with various roles
     // Test: getUsersByRole should return the expected list of addresses for each role
 
-    function test_GetUsersByRoleReturnsExpected() public {
-        // address[] memory expectedUsers = new address[](2);
-        // expectedUsers[0] = address(0x1);
-        // expectedUsers[1] = address(0x2);
-        // authority.setRole(expectedUsers[0], 1);
-        // authority.setRole(expectedUsers[1], 1);
-        // address[] memory users = authority.getUsersByRole(1);
-        // assertEq(users.length, expectedUsers.length, "Returned users length mismatch");
-        // for (uint i = 0; i < users.length; i++) {
-        //     assertEq(users[i], expectedUsers[i], "Returned user address mismatch");
-        // }
+    function test_GetUsersByRoleReturnsExpected(uint usersWithRole) public {
+        vm.assume(usersWithRole < 100);
+        uint8 testRole = 50;
+
+        // create users
+        address payable[] memory expectedUsers = _utils.createUsers(usersWithRole);
+        
+        // grant role to all users
+        vm.startPrank(defaultGovernance);
+        for (uint i = 0; i < expectedUsers.length; i++) {
+            authority.setUserRole(expectedUsers[i], testRole, true);
+        }
+        vm.stopPrank();
+
+        address[] memory actualUsers = authority.getUsersByRole(testRole);
+
+        assertEq(actualUsers.length, expectedUsers.length, "Returned users length mismatch");
+
+        for (uint i = 0; i < expectedUsers.length; i++) {
+            assertEq(actualUsers[i], address(expectedUsers[i]), "Returned user address mismatch");
+        }
     }
 
     // Setup: Assign a few addresses with various roles, then add and remove users
     // Test: getUsersByRole should return the updated list of addresses for each role after additions and removals
-    function test_GetUsersByRoleReturnsExpectedAfterAddingAndRemovingUsers() public {
-        // address[] memory expectedUsers = new address[](1);
-        // expectedUsers[0] = address(0x1);
-        // authority.setRole(expectedUsers[0], 1);
-        // authority.setRole(address(0x2), 1);
-        // authority.setRole(address(0x2), 0); // Remove user
-        // address[] memory users = authority.getUsersByRole(1);
-        // assertEq(users.length, expectedUsers.length, "Returned users length mismatch");
-        // for (uint i = 0; i < users.length; i++) {
-        //     assertEq(users[i], expectedUsers[i], "Returned user address mismatch");
+    function test_GetUsersByRoleReturnsExpectedAfterAddingAndRemovingUsers(uint usersWithRole) public {
+        // vm.assume(usersWithRole < 100);
+        // uint8 testRole = 50;
+
+        // // create users
+        // address payable[] memory expectedUsers = _utils.createUsers(usersWithRole);
+        
+        // // grant role to all users
+        // vm.startPrank(defaultGovernance);
+        // for (uint i = 0; i < expectedUsers.length; i++) {
+        //     authority.setUserRole(expectedUsers[i], testRole, true);
+        // }
+        // vm.stopPrank();
+
+        // // remove role from some users
+        // // generate random value between expectedUsers - 1 and 0
+        // uint usersToRemove = 0;
+        // uint numUsersExpected = expectedUsers.length - usersToRemove;
+
+        // // accumulate users that should have been removed into a new list  
+        // // create list of users that remain
+
+        // address[] memory actualUsers = authority.getUsersByRole(testRole);
+        // assertEq(actualUsers.length, expectedUsers.length - usersToRemove, "Returned users length mismatch");
+
+        // // assert each user that was removed is not in the actual list
+        // // asert each user that was not removed is in the actual list
+        // for (uint i = 0; i < actualUsers.length; i++) {
+        //     assertEq(actualUsers[i], expectedUsers[i], "Returned user address mismatch");
         // }
     }
 
