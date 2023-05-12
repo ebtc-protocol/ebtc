fc6e3df07032c62045709a8331e14fcb4201f9cc
Entreprenerd <alex@entreprenerd.xyz>
2023-05-12 17:05:35 +0200

    feat: enforce owner test

--- packages/contracts/foundry_test/SimplifiedDiamondLikeLeverage.t.sol
+++ packages/contracts/foundry_test/SimplifiedDiamondLikeLeverage.t.sol
@@ -5,7 +5,25 @@ import "forge-std/Test.sol";
 import {SimplifiedDiamondLike} from "../contracts/SimplifiedDiamondLike.sol";
 
 import {eBTCBaseInvariants} from "./BaseInvariants.sol";
-import {LeverageMacroReference} from "../contracts/LeverageMacroReference.sol";
+import {LeverageMacroDelegateTarget} from "../contracts/LeverageMacroDelegateTarget.sol";
+
+
+
+interface IOwnerLike {
+    function owner() external view returns (address);
+}
+
+// Basic test to ensure delegate call will maintain the owner as we'd expect
+contract LeverageMacroOwnerCheck {
+    event DebugOwner(address);
+    function getOwner() external returns (address) {
+        // Since we delegate call we should have access to this if we call ourselves back
+        address owner = IOwnerLike(address(this)).owner();
+        emit DebugOwner(owner);
+
+        return owner;
+    }
+}
 
 contract SimplifiedDiamondLikeLeverageTests is eBTCBaseInvariants {
     mapping(bytes32 => bool) private _cdpIdsExist;
@@ -15,10 +33,16 @@ contract SimplifiedDiamondLikeLeverageTests is eBTCBaseInvariants {
     bytes32 public constant DUMMY_CDP_ID = bytes32(0);
     uint public constant INITITAL_COLL = 10000 ether;
 
-    address user_1 = address(0xbad455);
+    address user = address(0xbad455);
 
-    function _createNewWalletForUser(address user) internal returns (address payable) {
-        SimplifiedDiamondLike contractWallet = new SimplifiedDiamondLike(user);
+    LeverageMacroDelegateTarget macro_reference;
+    LeverageMacroOwnerCheck owner_check;
+
+    SimplifiedDiamondLike wallet;
+
+
+    function _createNewWalletForUser(address _user) internal returns (address payable) {
+        SimplifiedDiamondLike contractWallet = new SimplifiedDiamondLike(_user);
         return payable(contractWallet);
     }
 
@@ -27,17 +51,29 @@ contract SimplifiedDiamondLikeLeverageTests is eBTCBaseInvariants {
 
         connectCoreContracts();
         connectLQTYContractsToCore();
-    }
 
-    function test_happyOpen() public {
-        address user = _utils.createUsers(1)[0];
+        macro_reference = new LeverageMacroDelegateTarget(
+        address(borrowerOperations),
+        address(activePool),
+        address(cdpManager),
+        address(eBTCToken),
+        address(collateral),
+        address(sortedCdps)
+        );
+        owner_check = new LeverageMacroOwnerCheck();
+
+
         vm.deal(user, type(uint96).max);
 
         // check input
         uint256 netColl = 1e20;
         // deploy proxy for user
-        SimplifiedDiamondLike wallet = SimplifiedDiamondLike(_createNewWalletForUser(user));
+        wallet = SimplifiedDiamondLike(_createNewWalletForUser(user));
 
+        uint256 collBall = dealCollateral(user, netColl);
+    }
+
+    function test_happyOpen() public {
         /// == OPEN CDP FLOW == ///
         // User:
         // Approve the Wallet
@@ -47,13 +83,12 @@ contract SimplifiedDiamondLikeLeverageTests is eBTCBaseInvariants {
         // TransferFrom
         // OpenCdp
         // Transfer To user
-        uint256 collBall = dealCollateral(user, netColl);
+        
 
         // User.Approve col
         vm.startPrank(user);
         collateral.approve(address(wallet), type(uint256).max);
-
-        assertEq(collBall, collateral.balanceOf(user), "User bal");
+        uint256 collBall = collateral.balanceOf(user);
 
         // In macro
         // TransferFrom
@@ -127,15 +162,37 @@ contract SimplifiedDiamondLikeLeverageTests is eBTCBaseInvariants {
 
     }
 
-    function test_openWithLeverage() public {
-        // TODO: 
+    function test_ownerCheckWithDelegateCall() public {
+        SimplifiedDiamondLike.Operation[] memory data = new SimplifiedDiamondLike.Operation[](1);
 
+        // TransferFrom
+        data[0] = SimplifiedDiamondLike.Operation({
+                to: address(address(owner_check)),
+                checkSuccess: true,
+                value: 0,
+                gas: 9999999,
+                capGas: false,
+                opType: SimplifiedDiamondLike.OperationType.delegatecall,
+                data: abi.encodeCall(owner_check.getOwner, ())
+        });
+
+        vm.startPrank(user);
+        wallet.execute(data);
+        vm.stopPrank();
+
+        // TODO: Add test here to check event matches
+        
+    }
+
+    function test_openWithLeverage() public {
         // Same as above, but:
         // Swap data
         // Flashloan
         // Etc..
 
         // PROB Change the macro to not sweep
+
+        // Set the macro for callback
 
         // Create the Sweep Module
         
