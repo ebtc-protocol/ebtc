a587dd5072a8280a0797c189c915cec272d7b4fe
Entreprenerd <alex@entreprenerd.xyz>
2023-05-25 18:13:08 +0200

    fix: maxFlashLoan

--- packages/contracts/contracts/BorrowerOperations.sol
+++ packages/contracts/contracts/BorrowerOperations.sol
@@ -850,7 +850,8 @@ contract BorrowerOperations is
         bytes calldata data
     ) external override returns (bool) {
         require(amount > 0, "BorrowerOperations: 0 Amount");
-        require(token == address(ebtcToken), "BorrowerOperations: EBTC Only");
+        require(amount < maxFlashLoan(token), "BorrowerOperations: aboveMax");
+        // NOTE: Check for `eBTCToken` is implicit in the two requires above
 
         uint256 fee = (amount * feeBps) / MAX_BPS;
 
@@ -882,7 +883,7 @@ contract BorrowerOperations is
     }
 
     /// @dev Max flashloan, exclusively in ETH equals to the current balance
-    function maxFlashLoan(address token) external view override returns (uint256) {
+    function maxFlashLoan(address token) public view override returns (uint256) {
         if (token != address(ebtcToken)) {
             return 0;
         }
