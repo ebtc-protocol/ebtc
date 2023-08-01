9ea8a4fccb9d84849728de3cd85ae69fe2940414
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-08-01 14:20:46 -0300

    Add note to list of properties

--- packages/contracts/contracts/TestContracts/invariants/Properties.sol
+++ packages/contracts/contracts/TestContracts/invariants/Properties.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.17;
 import {ICollateralToken} from "../../Dependencies/ICollateralToken.sol";
 import {ActivePool} from "../../ActivePool.sol";
 
+// See PROPERTIES.md for the full list of invariants
 abstract contract Properties {
   function invariant_AP_01(ICollateralToken collateral, ActivePool activePool) internal view returns(bool) {
     return (collateral.sharesOf(address(activePool)) >= activePool.getStEthColl());
