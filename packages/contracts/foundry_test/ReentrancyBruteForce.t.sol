4680eb0588a43f8bcb6d3f93e0355b0d946f7392
Entreprenerd <alex@entreprenerd.xyz>
2023-08-01 15:32:45 +0200

    feat: more realistic base setup

--- packages/contracts/foundry_test/ReentrancyBruteForce.t.sol
+++ packages/contracts/foundry_test/ReentrancyBruteForce.t.sol
@@ -2,71 +2,80 @@
 pragma solidity 0.8.17;
 
 import "forge-std/Test.sol";
+import {eBTCBaseFixture} from "./BaseFixture.sol";
 
 contract ReentrantBruteforcer is Test {
 
-  function startReentrancy() external {
-    revert("TODO");
-    // You have to set this up
+  address exploitTarget;
+  bytes exploitData;
+
+  constructor(address target, bytes memory data) {
+    // Set them up
+    exploitTarget = target;
+    exploitData = data;
   }
 
-  uint256 reEntrancyContractSelector;
-  uint256 reentrancyFunctionSelector;
-  bool isSetup; 
+  function startReentrancy(address startTarget, bytes memory startCalldata) external returns (bool, bytes memory) {
+    (bool success, bytes memory retval) = startTarget.call(startCalldata);
 
-  function setupCallback(uint256 entropyTarget, uint256 entropyFunction) external{
-    isSetup = true;
-
-    // Pick Contract
-    reEntrancyContractSelector = entropyTarget;
-    reentrancyFunctionSelector = entropyFunction;
+    return (success, retval);
   }
 
-  // Pick Function from Contract
-
-  // for each contract
-  function contractPicker(uint256 contractIndex) {
-
-    contractIndex = contractsIndex.length; // VAR
-    
-    // Modulo logic from TS
-
-  }
-  function CONTRACT_NAME_Function_Picker(uint256 ) {
-      // First Modulo for Contract to Pick
-
-      // Second Modulo for Function to Call (with Contract)
-
-      // Each Contract call with individual default value
-  }
 
   function _handleFallback() internal {
-    address contractToCall = 
+    exploitTarget.call(exploitData); // I think this should revert either way
   }
 
 
   fallback() external payable {
-    require(isSetup, "Not Setup, tests are not properly set");
-    handleFallback();
+    _handleFallback();
   }
     
 }
 
+contract FakeReentrancyGuardTool {
+  uint256 lock;
+
+  function doTheOp() external {
+    if(lock == 0) {
+      lock = 1;
+      payable(msg.sender).call{value: address(this).balance}(""); // Trigger reentrancy here
+    } else {
+      revert("Error");
+    }
+  }
+
+  receive() external payable {
+
+  }
+}
+
 contract SCTestBasic is Test {
 
-  ReentrantBruteforcer c;
+  // Maybe we set it up here
+  // At the end of the day the reentrant brute forcer just needs address + calldata
 
-  function setUp() {
-    c = new ReentrantBruteforcer()
+  ReentrantBruteforcer c;
+  FakeReentrancyGuardTool demoTarget;
+
+  function setUp() public {
+    demoTarget = new FakeReentrancyGuardTool();
   }
 
 
-  function testBruteForceReentrancies(uint256 entropyTarget, uint256 entropyFunction) {
+  function testBruteForceReentrancies(uint256 entropyTarget, uint256 entropyFunction) public {
 
-    // Setup for fuzzing
-    c.setupCallback(entropyTarget, entropyFunction);
+    // On each iteration we deploy a new contract for exploit
+    c = new ReentrantBruteforcer(address(demoTarget), abi.encodeCall(demoTarget.doTheOp, ()));
+
+    vm.deal(address(this), 1);
+    payable(address(c)).call{value: 1}("");
 
     // You must set this up
-    c.startReentrancy();
+    (bool s, bytes memory retval) = c.startReentrancy(address(demoTarget), abi.encodeCall(demoTarget.doTheOp, ()));
+    assertEq(s, false); // Must have reverted
+    string memory expectedVal = "Error";
+    bytes4 errorString = 0x08c379a0; // This is added by Solidity compiler: https://trustchain.medium.com/reversing-and-debugging-evm-the-end-of-time-part-4-3eafe5b0511a
+    assertEq(retval, bytes.concat(errorString, abi.encode(expectedVal))); // Error must match
   }
 }
