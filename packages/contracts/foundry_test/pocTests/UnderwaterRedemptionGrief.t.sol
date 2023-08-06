cfb79bda6295cbb6e04d4bef016243479ed0f02f
dapp-whisperer <dapp@badger.com>
2023-08-06 14:48:08 +0200

    draft progress

--- packages/contracts/foundry_test/pocTests/UnderwaterRedemptionGrief.t.sol
+++ packages/contracts/foundry_test/pocTests/UnderwaterRedemptionGrief.t.sol
@@ -4,9 +4,22 @@ pragma solidity 0.8.17;
 import {console2 as console} from "forge-std/console2.sol";
 
 import {eBTCBaseInvariants} from "../BaseInvariants.sol";
+import {LogUtils} from "../utils/LogUtils.sol";
 
-contract RedemptionAdjustCdpGriefTest is eBTCBaseInvariants {
+contract RedemptionAdjustCdpGriefTest is eBTCBaseInvariants, LogUtils {
     address payable[] users;
+
+    struct CdpData {
+        uint status;
+        uint stake;
+        uint debt;
+        uint coll;
+        uint liqShares;
+        uint cachedPpfs;
+        uint redistributionIndex;
+        uint price;
+        uint ICR;
+    }
 
     function setUp() public override {
         super.setUp();
@@ -15,7 +28,18 @@ contract RedemptionAdjustCdpGriefTest is eBTCBaseInvariants {
         connectLQTYContractsToCore();
     }
 
-    function testRetryFullLiquidation() public {
+    function testGriefRealisticValues() public {
+        uint cdpShares = 1762804390723562871;
+        uint stEthPerShare = 11345558307686524;
+        uint debt = 135054545454545454;
+
+        uint cdpStEthBalance = cdpShares * stEthPerShare / 1e18;
+        
+        uint price = cdpStEthBalance * 1e18 / 135054545454545454;
+
+        console.log("price: ", price);
+    }
+    function testRedemptionGrief() public {
         address whale = _utils.getNextUserAddress();
         address degen1 = _utils.getNextUserAddress();
         address degen2 = _utils.getNextUserAddress();
@@ -40,12 +64,48 @@ contract RedemptionAdjustCdpGriefTest is eBTCBaseInvariants {
         console.log("TCR after degens only: ", cdpManager.getTCR(price));
 
         // 3) stETH share value drops to 1 ether - 1 wei
-        collateral.setEthPerShare(1e18-1);
+        collateral.setEthPerShare(2e17);
 
         // 4) users can adjust their CDP to have coll < 2 stETH as much as they want and create "dust CDPs"
         vm.prank(degen1);
         borrowerOperations.withdrawColl(degen1Cdp, 99 ether, bytes32(0), bytes32(0));
+    }
 
-        
+    function _getCdpData(bytes32 cdpId) public returns (CdpData memory) {
+        /**
+        coll (shares and current balance value)
+        debt 
+        liquidatorRewardShares (shares and current balance value)
+        ICR 
+        price
+         */
+
+        // collateral
+        // debt
+        // liquidatorRewardShares
+        // ICR
+        // price
+
+        uint status = cdpManager.getCdpStatus(cdpId);
+        uint stake = cdpManager.getCdpStake(cdpId);
+        uint debt = cdpManager.getCdpDebt(cdpId);
+        uint coll = cdpManager.getCdpColl(cdpId);
+        uint liqShares = cdpManager.getCdpLiquidatorRewardShares(cdpId);
+        uint cachedPpfs = cdpManager.stFeePerUnitcdp(cdpId);
+        uint redistributionIndex = cdpManager.rewardSnapshots(cdpId);
+        uint price = priceFeedMock.fetchPrice();
+        uint ICR = cdpManager.getCurrentICR(cdpId, price);
+
+        CdpData memory cdpData = CdpData(
+            status,
+            stake,
+            debt,
+            coll,
+            liqShares,
+            cachedPpfs,
+            redistributionIndex,
+            price,
+            ICR
+        );
     }
 }
