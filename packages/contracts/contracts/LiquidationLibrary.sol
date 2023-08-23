698c87f7b89fb9914c9df8667fd80070bb734819
Entreprenerd <alex@entreprenerd.xyz>
2023-08-23 15:38:38 +0200

    fix: nice error message when nothing to liquidate

--- packages/contracts/contracts/LiquidationLibrary.sol
+++ packages/contracts/contracts/LiquidationLibrary.sol
@@ -578,6 +578,7 @@ contract LiquidationLibrary is CdpManagerStorage {
         bytes32[] memory _batchedCdps;
         if (vars.recoveryModeAtStart) {
             _batchedCdps = _sequenceLiqToBatchLiq(_n, true, vars.price);
+            require(_batchedCdps.length > 0, "LiquidationLibrary: nothing to liquidate");
             totals = _getTotalFromBatchLiquidate_RecoveryMode(
                 vars.price,
                 systemColl,
@@ -588,6 +589,7 @@ contract LiquidationLibrary is CdpManagerStorage {
         } else {
             // if !vars.recoveryModeAtStart
             _batchedCdps = _sequenceLiqToBatchLiq(_n, false, vars.price);
+            require(_batchedCdps.length > 0, "LiquidationLibrary: nothing to liquidate");
             totals = _getTotalsFromBatchLiquidate_NormalMode(vars.price, _TCR, _batchedCdps, true);
         }
 
