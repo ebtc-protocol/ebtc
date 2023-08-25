47d56d40775edaf2cc12feceb8d12ece11290812
Entreprenerd <alex@entreprenerd.xyz>
2023-08-25 13:14:11 +0200

    feat: test that works (see commented code)

--- packages/contracts/foundry_test/GracePeriod.Sync.t.sol
+++ packages/contracts/foundry_test/GracePeriod.Sync.t.sol
@@ -31,7 +31,7 @@ contract GracePeriodBaseTests is eBTCBaseFixture {
         // Adjust
         // Close
 
-        _openRiskyCdps(1);
+        bytes32[] memory cdps = _openRiskyCdps(1);
 
 
         // Adjust and close one cdp
@@ -54,8 +54,83 @@ contract GracePeriodBaseTests is eBTCBaseFixture {
         emit TCRNotified(EXPECTED_TCR);
         _partialRedemption(1e17, price);
 
+        // Trigger Liquidations via Split (so price is constant)
+        _triggerRMViaSplit();
+        cdpManager.beginRMLiquidationCooldown();
+        vm.warp(block.timestamp + cdpManager.waitTimeFromRMTriggerToLiquidations() + 1);
 
         // Liquidate 4x
+        vm.startPrank(safeUser);
+        uint256 liquidationSnapshotId = vm.snapshot(); // New snap for liquidations
+
+        // == Liquidation 1 == //
+        console.log("Liq 1");
+
+        // Try liquidating a cdp
+        cdpManager.liquidate(cdps[0]);
+        // Get TCR after Liquidation
+        uint256 EXPECTED_TCR_FIRST_LIQ = cdpManager.getTCR(price);
+        // Revert so we can verify Event
+        vm.revertTo(liquidationSnapshotId); 
+        
+        // Verify it worked
+        vm.expectEmit(false, false, false, true);
+        emit TCRNotified(EXPECTED_TCR_FIRST_LIQ);
+        cdpManager.liquidate(cdps[0]);
+        
+
+        // == Liquidate 2 == //
+        console.log("Liq 2");
+
+        // Re-revert for next Op
+        vm.revertTo(liquidationSnapshotId);
+
+        // Try liquidating a cdp partially
+        cdpManager.partiallyLiquidate(cdps[0], 1e18, cdps[0], cdps[0]);
+        uint256 EXPECTED_TCR_SECOND_LIQ = cdpManager.getTCR(price);
+        vm.revertTo(liquidationSnapshotId);
+
+        // Verify it worked
+        vm.expectEmit(false, false, false, true);
+        emit TCRNotified(EXPECTED_TCR_SECOND_LIQ);
+        cdpManager.partiallyLiquidate(cdps[0], 1e18, cdps[0], cdps[0]);
+
+
+        // == Liquidate 3 == //
+        console.log("Liq 3");
+
+        // Re-revert for next Op
+        vm.revertTo(liquidationSnapshotId);
+
+        // Try liquidating a cdp via the list (1)
+        cdpManager.liquidateCdps(1);
+        uint256 EXPECTED_TCR_THIRD_LIQ = cdpManager.getTCR(price);
+        vm.revertTo(liquidationSnapshotId);
+
+        // Verify it worked
+        vm.expectEmit(false, false, false, true);
+        emit TCRNotified(EXPECTED_TCR_THIRD_LIQ);
+        cdpManager.liquidateCdps(1);
+
+        // == Liquidate 4 == //
+        console.log("Liq 4");
+
+        // Re-revert for next Op
+        vm.revertTo(liquidationSnapshotId);
+
+        // Try liquidating a cdp via the list (2)
+        bytes32[] memory cdpsToLiquidateBatch = new bytes32[](1);
+        cdpsToLiquidateBatch[0] = cdps[0];
+        cdpManager.batchLiquidateCdps(cdpsToLiquidateBatch);
+        uint256 EXPECTED_TCR_FOURTH_LIQ = cdpManager.getTCR(price);
+        vm.revertTo(liquidationSnapshotId);
+
+        vm.expectEmit(false, false, false, true);
+        emit TCRNotified(EXPECTED_TCR_FOURTH_LIQ);
+        cdpManager.batchLiquidateCdps(cdpsToLiquidateBatch);
+        vm.revertTo(liquidationSnapshotId);
+
+        vm.stopPrank();
     }
 
     function _partialRedemption(uint256 toRedeem, uint256 price) internal {
@@ -100,13 +175,13 @@ contract GracePeriodBaseTests is eBTCBaseFixture {
 
         uint256 _curPrice = priceFeedMock.getPrice();
 
-        bytes32[] memory cdps = new bytes32[](numberOfCdpsAtRisk + 1);
+        bytes32[] memory cdps = new bytes32[](numberOfCdpsAtRisk);
 
         // At risk CDPs (small CDPs)
         for (uint256 i; i < numberOfCdpsAtRisk; i++) {
             uint256 debt2 = 2e18;
             uint256 coll2 = _utils.calculateCollAmount(debt2, _curPrice, 1.15e18); // Fairly risky
-            cdps[1 + i] = _openTestCDP(users[0], coll2, debt2);
+            cdps[i] = _openTestCDP(users[0], coll2, debt2);
         }
 
         uint TCR = cdpManager.getTCR(_curPrice);
@@ -116,5 +191,15 @@ contract GracePeriodBaseTests is eBTCBaseFixture {
         vm.warp(cdpManager.getDeploymentStartTime() + cdpManager.BOOTSTRAP_PERIOD());
 
         return cdps;
+    }
+
+    function _triggerRMViaSplit() internal {
+        // 4% Downward Price will trigger RM but not Liquidations
+        collateral.setEthPerShare((collateral.getSharesByPooledEth(1e18) * 96) / 100); // 4% downturn, 5% should be enough to liquidate in-spite of RM
+        uint256 price = priceFeedMock.getPrice();
+
+        // Check if we are in RM
+        uint256 TCR = cdpManager.getTCR(price);
+        assertLt(TCR, 1.25e18, "!RM");
     }
 }
