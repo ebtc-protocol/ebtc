31b8f7423dc4433274b80b5d6f92441d9daa1925
dapp-whisperer <dapp-whisperer@pm.me>
2024-01-09 18:43:12 -0500

    fix

--- packages/contracts/foundry_test/utils/LogUtils.sol
+++ packages/contracts/foundry_test/utils/LogUtils.sol
@@ -49,7 +49,45 @@ contract LogUtils {
         return result;
     }
 
-    function bytes32ToString(bytes32 _bytes32) public pure returns (string memory) {
-        return _bytes32.bytes32ToString();
+    function parseNode(bytes32 node) public pure returns (address, uint256) {
+        bytes20 addressPart;
+        uint256 numberPart;
+
+        // Extract the first 20 bytes for the address
+        assembly {
+            addressPart := and(node, 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF000000000000000000000000)
+        }
+
+        // Extract the remaining 12 bytes for the uint256
+        assembly {
+            numberPart := and(node, 0x000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF)
+        }
+
+        return (address(addressPart), numberPart);
+    }
+
+    function bytes32ToString(bytes32 data) public pure returns (string memory result) {
+        bytes memory temp = new bytes(65);
+        uint256 count;
+
+        for (uint256 i = 0; i < 32; i++) {
+            bytes1 currentByte = bytes1(data << (i * 8));
+            
+            uint8 c1 = uint8(
+                bytes1((currentByte << 4) >> 4)
+            );
+            
+            uint8 c2 = uint8(
+                bytes1((currentByte >> 4))
+            );
+        
+            if (c2 >= 0 && c2 <= 9) temp[++count] = bytes1(c2 + 48);
+            else temp[++count] = bytes1(c2 + 87);
+            
+            if (c1 >= 0 && c1 <= 9) temp[++count] = bytes1(c1 + 48);
+            else temp[++count] = bytes1(c1 + 87);
+        }
+        
+        result = string(temp);
     }
 }
