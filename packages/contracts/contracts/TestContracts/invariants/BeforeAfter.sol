98c7e8c95128c5a1bc9c35cbc5b7ec1e1214cfce
dapp-whisperer <dapp-whisperer@pm.me>
2024-01-10 14:26:55 -0500

    lint

--- packages/contracts/contracts/TestContracts/invariants/BeforeAfter.sol
+++ packages/contracts/contracts/TestContracts/invariants/BeforeAfter.sol
@@ -217,10 +217,16 @@ abstract contract BeforeAfter is BaseStorageVariables, LogUtils {
         console.log("totalCollateralSnapshot         :", cdpManager.totalCollateralSnapshot());
         console.log("");
         console.log("systemStEthFeePerUnitIndex      :", cdpManager.systemStEthFeePerUnitIndex());
-        console.log("systemStEthFeePerUnitIndexError :", cdpManager.systemStEthFeePerUnitIndexError());
+        console.log(
+            "systemStEthFeePerUnitIndexError :",
+            cdpManager.systemStEthFeePerUnitIndexError()
+        );
         console.log("");
         console.log("systemDebtRedistributionIndex   :", cdpManager.systemDebtRedistributionIndex());
-        console.log("lastEBTCDebtErrorRedistribution :", cdpManager.lastEBTCDebtErrorRedistribution());
+        console.log(
+            "lastEBTCDebtErrorRedistribution :",
+            cdpManager.lastEBTCDebtErrorRedistribution()
+        );
         console.log("");
         console.log("price                           :", price);
         console.log("");
@@ -236,22 +242,31 @@ abstract contract BeforeAfter is BaseStorageVariables, LogUtils {
             (uint256 debtSynced, uint256 collSharesSynced) = cdpManager.getSyncedDebtAndCollShares(
                 node
             );
-            
+
             _printCdpId("=== ", node);
             console.log("owner                       :", sortedCdps.getOwnerAddress(node));
             console.log("debt       (realized)       :", cdpManager.getCdpDebt(node));
             console.log("debt       (virtual)        :", debtSynced);
             console.log("collShares (realized)       :", cdpManager.getCdpCollShares(node));
-            console.log("collShares (virtual)        :", collSharesSynced);            
+            console.log("collShares (virtual)        :", collSharesSynced);
             console.log("");
             console.log("ICR        (realized)       :", cdpManager.getCachedICR(node, price));
             console.log("ICR        (virtual)        :", cdpManager.getSyncedICR(node, price));
             console.log("");
             console.log("stake                       :", cdpManager.getCdpStake(node));
-            console.log("cdpDebtRedistributionIndex  :", cdpManager.cdpDebtRedistributionIndex(node));
+            console.log(
+                "cdpDebtRedistributionIndex  :",
+                cdpManager.cdpDebtRedistributionIndex(node)
+            );
             console.log("cdpStEthFeePerUnitIndex     :", cdpManager.cdpStEthFeePerUnitIndex(node));
-            console.log("hasPendingRedistributedDebt :", cdpManager.hasPendingRedistributedDebt(node));
-            console.log("getPendingRedistributedDebt :", cdpManager.getPendingRedistributedDebt(node));
+            console.log(
+                "hasPendingRedistributedDebt :",
+                cdpManager.hasPendingRedistributedDebt(node)
+            );
+            console.log(
+                "getPendingRedistributedDebt :",
+                cdpManager.getPendingRedistributedDebt(node)
+            );
             console.log("");
             node = sortedCdps.getPrev(node);
         }
