6f87415f71b7fdaba0c8672a071470a261ce0d92
wtj2021 <wtj2022@proton.me>
2024-03-08 09:02:27 -0800

    extracting roundId from latestRoundData

--- packages/contracts/contracts/Dependencies/AggregatorV3Interface.sol
+++ packages/contracts/contracts/Dependencies/AggregatorV3Interface.sol
@@ -10,8 +10,6 @@ interface AggregatorV3Interface {
 
     function version() external view returns (uint256);
 
-    function latestRound() external view returns (uint80);
-
     // getRoundData and latestRoundData should both raise "No data present"
     // if they do not have data to report, instead of returning unset values
     // which could be misinterpreted as actual reported values.
