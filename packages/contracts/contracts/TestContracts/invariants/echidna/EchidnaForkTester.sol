6e6307003e1ecaf3903c10beb405f21829d88f38
Entreprenerd <alex@entreprenerd.xyz>
2024-03-15 20:37:02 +0100

    chore: move to top

--- packages/contracts/contracts/TestContracts/invariants/echidna/EchidnaForkTester.sol
+++ packages/contracts/contracts/TestContracts/invariants/echidna/EchidnaForkTester.sol
@@ -8,12 +8,14 @@ import "../TargetFunctions.sol";
 
 contract EchidnaForkTester is EchidnaAsserts, EchidnaProperties, TargetFunctions {
     constructor() payable {
+        // https://etherscan.io/tx/0xca4f2e9a7e8cc82969e435091576dbd8c8bfcc008e89906857056481e0542f23
+        hevm.roll(19442237); // Block
+        hevm.warp(1710528599);
+
         _setUpFork();
         _setUpActors();
         
-        // https://etherscan.io/tx/0xca4f2e9a7e8cc82969e435091576dbd8c8bfcc008e89906857056481e0542f23
-        hevm.roll(19437242); // Block
-        hevm.warp(1710460800);
+
     }
 
     function setPrice(uint256) public pure override {
